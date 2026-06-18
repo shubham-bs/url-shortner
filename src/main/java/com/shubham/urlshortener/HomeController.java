7dc24e12517f5a08ca95580509f89e74ae1dc9c6
Shubham <saymyname@Bags.local>
2026-06-18 20:40:13 +0530

    Rebuild project in IntelliJ IDEA

---
+++ src/main/java/com/shubham/urlshortener/HomeController.java
@@ -0,0 +1,20 @@
+package com.shubham.urlshortener;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("title","URL Shortener-Thymeleaf");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+}
